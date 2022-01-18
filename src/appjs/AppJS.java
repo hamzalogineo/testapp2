b82905916f9e66f3eea679a20fe4aa1e0a01a6f1
unknown <hamzadiakit@gmail.com>
2022-01-18 20:24:00 +0000

    18_01_2022 hamza

---
+++ src/appjs/AppJS.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appjs;
+
+import frontoffice.Connexion;
+
+/**
+ *
+ * @author hp
+ */
+public class AppJS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Connexion con = new Connexion() ;
+                  con.setVisible(true) ;
+        
+    }
+    
+}
